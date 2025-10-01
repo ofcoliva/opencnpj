851926113c56754be7c42d98ac39fb5df8e749d6
oliva <ofcoliva@gmail.com>
2025-09-30 22:48:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
-with open ("README.md", "r") as f:  readme = f.read()
+# with open ("README.md", "r") as f:  readme = f.read()
 
 with open("requirements.txt", "r", encoding="UTF-8") as f:
     lines = f.read().splitlines()
-    requirements = [line.strip() for line in lines if line and not line.startswith("#")]
-        
+    requirements = [line.strip() for line in lines if line and not line.startswith("#")]    
+
 setup(
     name="opencnpj",
     version="0.0.1",
     license="MIT License",
     author="ofcoliva",
-    long_description=readme,
+    long_description="Repository with more instructions available at: https://github.com/ofcoliva/opencnpj",
     long_description_content_type="text/markdown",
     author_email="ofcoliva@gmail.com",
     keywords="opencnpj api cnpj",
