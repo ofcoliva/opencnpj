1d637a873155983d9785ccff9380c223cb0eb059
oliva <ofcoliva@gmail.com>
2025-10-01 22:48:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup
 
-# with open ("README.md", "r") as f:  readme = f.read()
-
-with open("requirements.txt", "r", encoding="UTF-8") as f:
-    lines = f.read().splitlines()
-    requirements = [line.strip() for line in lines if line and not line.startswith("#")]    
-
 setup(
     name="opencnpj",
     version="0.0.1",
@@ -16,5 +10,5 @@ setup(
     author_email="ofcoliva@gmail.com",
     keywords="opencnpj api cnpj",
     packages=["opencnpj"],
-    install_requires=requirements
+    install_requires=["requests", "pip_system_certs"]
 )
