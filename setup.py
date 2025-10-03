e0ce9ecdd842024f441f26f90d4e705197ea8607
oliva <ofcoliva@gmail.com>
2025-10-03 11:10:15 -0300

    0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="opencnpj",
-    version="0.0.1",
+    version="0.0.2",
     license="MIT License",
     author="ofcoliva",
     description="Wrapper para a API OpenCNPJ",
